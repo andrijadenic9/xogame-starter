505deebc5a564767a20368345af96e5f5ae63b94
andrijadenic9 <andrijadenic9@gmail.com>
2024-02-20 15:44:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+	return (
+		<div className="app">
+			<h1 className="title">X-O Game</h1>
+
+			{/* // * Unutar 'squareContainer' idu sve moje celije */}
+			<div className="squareContainer"></div>
+
+			<button>Reset Game</button>
+
+			<h2>POBEDNIK JE X ILI O ILI JE NERESENO</h2>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Cell.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cell() {
+	return (
+		<div className="square">
+			// * Ovom divu se dodaju klase 'cross' ili 'circle'
+			<div></div>
+		</div>
+	);
+}
+
+export default Cell;
